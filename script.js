377d2d5e1b8a4532bbdcfafb9a9c55df9df03cdc
Hitesh Marwaha <73687191+hitu69@users.noreply.github.com>
2023-04-22 20:56:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function decimalToBinary(num){
-et b = 0;
-let i = 1;
-while (a != 0) {
-let rem = a % 2;
-b = b + rem * i;
-i = i * 10;
-a = Math.floor(a / 2);
+let binaryStr = ""
+while(num>0){
+let rem = num%2;
+binaryStr = binaryStr + rem.toString()
+
+num = Math.floor(num/2)
 }
-return b;
+
+return binaryStr.split("").reverse().join("")
 }
 
 module.exports = decimalToBinary;
